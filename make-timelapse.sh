662fd6c8ea794608133afb1ebddf759b8c61db7d
willjasen <brandon@willjasen.com>
2025-01-15 22:31:05 -0500

    append timestamp to filename

--- make-timelapse.sh
+++ make-timelapse.sh
@@ -2,12 +2,16 @@
 WIDTH=1920
 HEIGHT=1080
 CRF_QUALITY=32
+GLOB="2016/05/26/*.jpg"
+
+# Get the current timestamp
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 
 # Create the timelapse video using images from all subdirectories
-ffmpeg -pattern_type glob -i "*.jpg" \
+ffmpeg -pattern_type glob -i "2016/05/26/*.jpg" \
   -vf "scale=${WIDTH}:${HEIGHT}:force_original_aspect_ratio=decrease,pad=${WIDTH}:${HEIGHT}:(ow-iw)/2:(oh-ih)/2" \
   -r 24 \
   -c:v libx264 \
   -crf ${CRF_QUALITY} \
   -pix_fmt yuv420p \
-  _timelapse.mp4
+  _timelapse_${TIMESTAMP}.mp4
